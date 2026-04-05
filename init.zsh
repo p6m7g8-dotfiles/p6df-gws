cb2e21c35b07ca946327c41d69796ed9175851fb
Philip M. Gollucci <pgollucci@p6m7g8.com>
2026-04-04 20:55:29 -0400

    chore(init): reorder hooks per hook_api.md lifecycle order (#6)
    * chore(init): reorder hooks per hook_api.md lifecycle order
    
    * fix(init): preserve docblock pairing when reordering hooks

--- init.zsh
+++ init.zsh
@@ -16,19 +16,16 @@ p6df::modules::gws::deps() {
 ######################################################################
 #<
 #
-# Function: p6df::modules::gws::skills::init(_module, _dir)
+# Function: p6df::modules::gws::path::init()
 #
-#  Args:
-#	_module -
-#	_dir -
-#
+#  Environment:	 P6_DFZ_SRC_DIR
 #>
 ######################################################################
-p6df::modules::gws::skills::init() {
+p6df::modules::gws::path::init() {
+
   local _module="$1"
   local _dir="$2"
-
-  gws generate-skills
+  p6df::core::path::if "$P6_DFZ_SRC_DIR/googleworkspace/cli/target/release"
 
   p6_return_void
 }
@@ -51,23 +48,6 @@ p6df::modules::gws::langs() {
 ######################################################################
 #<
 #
-# Function: p6df::modules::gws::path::init()
-#
-#  Environment:	 P6_DFZ_SRC_DIR
-#>
-######################################################################
-p6df::modules::gws::path::init() {
-
-  local _module="$1"
-  local _dir="$2"
-  p6df::core::path::if "$P6_DFZ_SRC_DIR/googleworkspace/cli/target/release"
-
-  p6_return_void
-}
-
-######################################################################
-#<
-#
 # Function: words gws $GOOGLE_APPLICATION_CREDENTIALS = p6df::modules::gws::profile::mod()
 #
 #  Returns:
@@ -80,3 +60,23 @@ p6df::modules::gws::profile::mod() {
 
   p6_return_words 'gws' '$GOOGLE_APPLICATION_CREDENTIALS'
 }
+######################################################################
+#<
+#
+# Function: p6df::modules::gws::skills::init(_module, _dir)
+#
+#  Args:
+#	_module -
+#	_dir -
+#
+#>
+######################################################################
+p6df::modules::gws::skills::init() {
+  local _module="$1"
+  local _dir="$2"
+
+  gws generate-skills
+
+  p6_return_void
+}
+
